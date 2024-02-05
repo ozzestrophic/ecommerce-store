662f386430f36eb3c6d11026a0eca5f1e8c9f25f
Ozzeek <osama.m.alnagar@gmail.com>
2024-02-05 17:21:11 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+export default App;
