372dbf9e537d7c0ed73acbc6c57917a2f94cc4a0
Ozzeek <osama.m.alnagar@gmail.com>
2024-02-28 01:47:20 +0200

    feat: games fetch based on category

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,75 +1,88 @@
-import { useEffect, useState } from "react";
-// import supabase from "../../config/supabaseClient";
+import { useEffect, useRef, useState } from "react";
+import supabase from "../../config/supabaseClient";
 import Card from "../homePage/Card";
 import CategoriesSideBar from "./CategoriesSideBar";
 import { Link } from "react-router-dom";
 
 const Categories = () => {
-  const [games, setGames] = useState([]);
+  const [gamesList, setGamesList] = useState([]);
+  const [fetchError, setFetchError] = useState(null);
+  const [category, setCategory] = useState("Action RPG");
 
-  useEffect(() => {
-    const fetchGames = async () => {
-      const gamesList = await fetch("http://localhost:4000/games");
-      const gamesListArray = await gamesList.json();
-      setGames(gamesListArray);
-    };
+  const categoryId = useRef("");
+  const gamesListIds = useRef([]);
 
-    fetchGames();
-  }, []);
-
-  // const [category, setCategory] = useState("Action RPG");
-  // let [categoryId, setCategoryId] = useState("");
-  // let [gamesListIds, setGamesListIds] = useState([]);
-  // //   const [gamesList, setGamesList] = useState([]);
-  // const [fetchError, setFetchError] = useState(null);
-
+  // ============ fetch from local serve =============
   // useEffect(() => {
-  //   const fetchFunc = async () => {
-  //     const fetchId = async () => {
-  //       const supaCategoryId = await supabase
-  //         .from("categories")
-  //         .select("id")
-  //         .eq("category", category)
-  //         .single();
-
-  //       if (supaCategoryId.error) {
-  //         setFetchError("couldn't fetch games");
-  //         setCategoryId(null);
-  //         console.log(fetchError);
-  //       }
-  //       if (supaCategoryId.data) {
-  //         setCategoryId(supaCategoryId.data);
-  //         setFetchError(null);
-  //         console.log(categoryId);
-  //       }
-  //     };
-
-  //     const fetchGameIDs = async () => {
-  //       const supaGamesIDs = await supabase
-  //         .from("categoriesGamesRel")
-  //         .select("gameID")
-  //         .eq("categoryID", categoryId);
-
-  //       if (supaGamesIDs.error) {
-  //         setFetchError("couldn't fetch games");
-  //         setGamesListIds(null);
-  //         console.log(fetchError);
-  //       }
-  //       if (supaGamesIDs.data) {
-  //         setGamesListIds(supaGamesIDs.data);
-  //         setFetchError(null);
-  //         console.log(gamesListIds);
-  //       }
-  //     };
-
-  //     await fetchId();
-  //     await fetchGameIDs();
-
-  //     //   const supaGamesList = await supabase
+  //   const fetchGames = async () => {
+  //     const fetchedgames = await fetch("http://localhost:4000/games");
+  //     const gamesListArray = await fetchedgames.json();
+  //     setGamesList(gamesListArray);
   //   };
 
-  //   fetchFunc();
-  // }, [category]);
+  //   fetchGames();
+  // }, []);
+
+  // fetch from supabase =========
+
+  useEffect(() => {
+    const fetchId = async () => {
+      const supaCategoryId = await supabase
+        .from("categories")
+        .select("id")
+        .eq("category", category)
+        .single();
+
+      if (supaCategoryId.error) {
+        setFetchError("couldn't fetch category ID");
+        console.log(fetchError);
+      }
+      if (supaCategoryId.data) {
+        categoryId.current = supaCategoryId.data;
+        setFetchError(null);
+      }
+    };
+
+    const fetchGameIDs = async () => {
+      const supaGamesIDs = await supabase
+        .from("categoriesGamesRel")
+        .select("gameID")
+        .eq("categoryID", categoryId.current.id);
+
+      if (supaGamesIDs.error) {
+        setFetchError("couldn't fetch game IDs");
+        console.log(fetchError);
+      }
+      if (supaGamesIDs.data) {
+        let array = supaGamesIDs.data.map((data) => data.gameID);
+        gamesListIds.current = array;
+        setFetchError(null);
+      }
+    };
+
+    const fetchGames = async () => {
+      const supaGames = await supabase
+        .from("games")
+        .select()
+        .in("id", gamesListIds.current);
+
+      if (supaGames.error) {
+        setFetchError("couldn't fetch game IDs");
+        console.log(fetchError);
+      }
+      if (supaGames.data) {
+        setGamesList(supaGames.data);
+        setFetchError(null);
+      }
+    };
+    const fetchFunc = async () => {
+      await fetchId();
+      await fetchGameIDs();
+      await fetchGames();
+    };
+
+    fetchFunc();
+  }, [category]);
 
   return (
     <section>
@@ -77,9 +90,9 @@ const Categories = () => {
       <div className="flex gap-8">
         <CategoriesSideBar />
         <div className="flex flex-1 flex-wrap gap-4">
-          {games.map((game) => {
+          {gamesList.map((game) => {
             return (
-              <Link to={`game/${game.id}`} key={game.id}>
+              <Link to={`/game/${game.id}`} key={game.id}>
                 <Card game={game} />
               </Link>
             );
